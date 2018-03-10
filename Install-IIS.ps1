a02789af592f0be169f45f196710cbb8693f279e
Adam Bertram <adbertram@gmail.com>
2018-03-10 14:33:14 -0600

    updates

--- Install-IIS.ps1
+++ Install-IIS.ps1
@@ -1,5 +1,18 @@
 $ErrorActionPreference = 'Stop'
 
+if ($PSHOME -like "*SysWOW64*") {
+	Write-Warning "Restarting this script under 64-bit Windows PowerShell."
+
+	& (Join-Path ($PSHOME -replace "SysWOW64", "SysNative") powershell.exe) -File (Join-Path $PSScriptRoot $MyInvocation.MyCommand) @args
+
+	Exit $LastExitCode
+}
+
+# Was restart successful?
+Write-Warning "Hello from $PSHOME"
+Write-Warning "  (\SysWOW64\ = 32-bit mode, \System32\ = 64-bit mode)"
+Write-Warning "Original arguments (if any): $args"
+
 Import-Module ServerManager
 
 Add-WindowsFeature Web-Server
